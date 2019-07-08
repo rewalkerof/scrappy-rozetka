39495355916f960b66e2653288912e1f1af4d0cf
gda2048 <goncharovdma@gmail.com>
2019-07-09 00:12:28 +0300

    fix get_html

--- scrappy.py
+++ scrappy.py
@@ -6,8 +6,11 @@ BASE_URL = 'https://rozetka.com.ua/notebooks/c80004/filter/'
 
 
 def get_html(url=BASE_URL):
-    req = requests.get(url, timeout=10)
-    return req.text
+    try:
+        return requests.get(url, timeout=10).text
+    except requests.exceptions.ConnectionError:
+        print('Rozetka is unavailable')
+        exit()
 
 
 def get_soup(html, parser='html.parser', tags_to_delete=None):
@@ -27,13 +30,16 @@ def repeat_until_done(func):
                 return func(*args, **kwargs)
             except AttributeError:
                 print('Error. Trying to do it again.')
+
     return wrapped
 
 
 @repeat_until_done
 def get_count_pages(url=BASE_URL):
     soup = get_soup(get_html(url), tags_to_delete=('script',))
-    pages_anc = soup.find('nav', class_='paginator-catalog pos-fix').find_all('a', class_='blacklink paginator-catalog-l-link')[-1].get('href')
+    pages_anc = \
+    soup.find('nav', class_='paginator-catalog pos-fix').find_all('a', class_='blacklink paginator-catalog-l-link')[
+        -1].get('href')
     total = pages_anc.split('/')[-2].split('=')[1] or re.search(r'1\d{2}', pages_anc)[0]
     return int(total)
 
@@ -45,27 +51,22 @@ def get_page_data(url):
     product_catalog = soup.find('div', class_='g-i-tile-l g-i-tile-catalog-hover-left-side clearfix').find_all(
         'div', class_='g-i-tile g-i-tile-catalog')
     for item, product in enumerate(product_catalog, 1):
-        dct_data.setdefault(item, {})['title'] = product.find('div', class_='g-i-tile-i-title clearfix').find(
-            'a').next.strip()
-        dct_data.setdefault(item, {})['url'] = product.find('a').get('href')
+        product = {'title': product.find('div', class_='g-i-tile-i-title clearfix').find('a').next.strip(),
+                   'url': product.find('a').get('href')}
+        dct_data[item] = product
     return dct_data
 
 
 def main():
     total_pages = get_count_pages()
 
-    for page in range(1, total_pages+1):
+    for page in range(1, total_pages + 1):
         url = f'{BASE_URL}page={int(page)}/'
-        try:
-            dct_data = get_page_data(url)
-        except requests.exceptions.ConnectionError:
-            print('Rozetka is unavailable')
-            return
-
+        dct_data = get_page_data(url)
         print(f'\n\t\t\t\tPage: {page} ', end='\n\n')
 
         for item, value in dct_data.items():
-            print(''.join('\n{}. {} \n\turl: {}\n\tprice: None'.format(item, value['title'], value['url'])))
+            print('\n{}. {} \n\turl: {}\n\tprice: None'.format(item, value['title'], value['url']))
 
 
 if __name__ == '__main__':
